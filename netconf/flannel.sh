71b8c613a732ecc36fd2c5ce1b2d330ab5f78559
njuicsgz <gaozheng0123@163.com>
2015-02-18 08:41:39 -0800

    allow to access private git hub without secure

--- netconf/flannel.sh
+++ netconf/flannel.sh
@@ -8,4 +8,4 @@ flanneld -etcd-endpoints=http://allen01:4001 > /dev/null 2>&1 &
 sleep 1
 source /run/flannel/subnet.env
 ip link del docker0
-docker -d --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU}  > /dev/null 2>&1 &
+docker -d --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} --insecure-registry allen01:5000 > /dev/null 2>&1 &
