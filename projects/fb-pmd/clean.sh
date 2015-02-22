768701bc24622ffd13399d03dfeada5a63bf2591
njuicsgz <gaozheng0123@163.com>
2015-02-22 09:44:45 -0800

    add walle-web with Dockerfile and k8s

--- projects/fb-pmd/clean.sh
+++ projects/fb-pmd/clean.sh
@@ -9,6 +9,7 @@ kubectl resize replicationController walle-java --replicas=0
 kubectl resize replicationController dubbo-monitor-svc --replicas=0
 kubectl resize replicationController dubbo-monitor-web --replicas=0
 kubectl resize replicationController config --replicas=0
+kubectl resize replicationController walle-web --replicas=0
 
 sleep 3 
 
@@ -16,12 +17,14 @@ kubectl delete replicationController mq-service
 kubectl delete replicationController fbagent-service
 kubectl delete replicationController scheduler
 kubectl delete replicationController walle-java
+kubectl delete replicationController walle-web
 kubectl delete replicationController rdb-service
 kubectl delete replicationController dubbo-monitor-svc
 kubectl delete replicationController dubbo-monitor-web
 kubectl delete replicationController config
 
 kubectl delete service walle-java
+kubectl delete service walle-web
 kubectl delete service dubbo-monitor-svc
 kubectl delete service dubbo-monitor-web
 kubectl delete service config
--- projects/fb-pmd/create.sh
+++ projects/fb-pmd/create.sh
@@ -6,10 +6,12 @@ kubectl create -f mq-services-rc.json
 kubectl create -f rdb-rc.json
 kubectl create -f scheduler-rc.json
 kubectl create -f walle-java-rc.json
-kubectl create -f walle-java-svc.json
+#kubectl create -f walle-java-svc.json
 kubectl create -f dubbo-monitor-svc-rc.json
 kubectl create -f dubbo-monitor-svc-svc.json
 kubectl create -f dubbo-monitor-web-rc.json
 kubectl create -f dubbo-monitor-web-svc.json
 kubectl create -f config-rc.json
 kubectl create -f config-svc.json
+kubectl create -f walle-web-rc.json
+kubectl create -f walle-web-svc.json
