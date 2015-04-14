29e88479bd1c2d2cfb0e2976d6f9fb04d2253c15
Gao Zheng <gaozheng012@163.com>
2015-04-14 02:00:40 +0000

    modify create.sh for project paas

--- clusters/aws-us-east-1/projects/paas/create.sh
+++ clusters/aws-us-east-1/projects/paas/create.sh
@@ -4,6 +4,7 @@ kubectl config set current-context my-context
 kubectl config set-context my-context --namespace=paas
 
 kubectl create -f registry-rc.json
+sleep 10 
 kubectl create -f registry-svc.json
 sleep 2
 kubectl create -f registry-proxy-rc.json
