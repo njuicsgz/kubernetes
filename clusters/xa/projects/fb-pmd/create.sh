a3cc667e9a1ee8e5fca354bc7752e2fc9d511b1c
Gao Zheng <gaozheng0123@163.com>
2015-04-17 16:18:31 +0800

    create zk by k8s

--- clusters/xa/projects/fb-pmd/create.sh
+++ clusters/xa/projects/fb-pmd/create.sh
@@ -3,6 +3,19 @@
 kubectl config set current-context my-context
 kubectl config set-context my-context --namespace=facebook-pmd
 #kubecfg ns facebook-pmd
+kubectl create -f zk-rc.json 
+kubectl create -f zk-svc.json 
+
+echo "ZK is crated."
+sleep 15
+
+kubectl create -f config-rc.json
+kubectl create -f config-svc.json
+echo "creating configuration center..."
+sleep 15
+echo "you MUST to push all config to ZK before starting other components. And you have 1min to operate this."
+
+sleep 60 
 kubectl create -f fbagent-service-rc.json
 kubectl create -f mq-services-rc.json
 kubectl create -f rdb-rc.json
@@ -13,7 +26,5 @@ kubectl create -f dubbo-monitor-svc-rc.json
 kubectl create -f dubbo-monitor-svc-svc.json
 kubectl create -f dubbo-monitor-web-rc.json
 kubectl create -f dubbo-monitor-web-svc.json
-kubectl create -f config-rc.json
-kubectl create -f config-svc.json
 kubectl create -f walle-web-rc.json
 kubectl create -f walle-web-svc.json
